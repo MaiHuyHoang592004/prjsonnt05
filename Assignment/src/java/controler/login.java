6d5cd7831825719deb2af471af12da5931884749
Hoang Mai Huy <huyhoang5924@gmail.com>
2024-11-02 03:16:03 +0700

    tạo project mới
    -tạo một packet controler và servlet login cơ bản

---
+++ Assignment/src/java/controler/login.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author huyho
+ */
+@WebServlet(name="login", urlPatterns={"/login"})
+public class login extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Đăng nhập - Công ty ABC</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Công TY ABC</h1>");
+            out.println("<form action=\"/login\" method=\"post\">");
+            out.println("<label for=\"username\">Tên đăng nhập:</label><br>");
+            out.println("<input type=\"text\" id=\"username\" name=\"username\"><br>");
+            out.println("<label for=\"password\">Mật khẩu:</label><br>");
+            out.println("<input type=\"password\" id=\"password\" name=\"password\"><br><br>");
+            out.println("<input type=\"submit\" value=\"Đăng nhập\">");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Servlet for ABC company HR management login page";
+    }
+}
