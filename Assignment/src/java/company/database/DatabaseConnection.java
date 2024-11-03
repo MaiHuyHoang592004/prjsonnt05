15fa3b38c0a78234938f9ddbd5ee70a584f378af
Hoang Mai Huy <huyhoang5924@gmail.com>
2024-11-03 21:24:46 +0700

    thêm phương thức để tets xem kết nối sql thành công hay thất bại
    - thất bại

--- Assignment/src/java/company/database/DatabaseConnection.java
+++ Assignment/src/java/company/database/DatabaseConnection.java
@@ -5,11 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:ADMIN://localhost:1433;databaseName=prj03";
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=prj03";
     private static final String USER = "sa";
     private static final String PASSWORD = "123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void testConnection() {
+        try (Connection connection = getConnection()) {
+            if (connection != null) {
+                System.out.println("Kết nối thành công!");
+            } else {
+                System.out.println("Kết nối thất bại!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Kết nối thất bại!");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        testConnection();
     }
 }
