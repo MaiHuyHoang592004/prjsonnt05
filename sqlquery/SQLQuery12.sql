6d92068f028c63941212838a56d8c3e7c56086ac
Hoang Mai Huy <huyhoang5924@gmail.com>
2024-11-03 16:33:01 +0700

    xóa code đê thay đổi cấu trúc chương trình để thuận tiện phát triển về sau
    src/
    ├── controller/
    │   ├── accesscontrol/
    │   │   ├── BaseRBACController.java
    │   │   └── LoginController.java
    │   ├── employee/
    │   │   ├── EmployeeCreateController.java
    │   │   ├── EmployeeDeleteController.java
    │   │   ├── EmployeeListController.java
    │   │   ├── EmployeeUpdateController.java
    │   │   └── EmployeeSearchController.java
    │   ├── productionplan/
    │   │   ├── ProductionPlanCreateController.java
    │   │   ├── ProductionPlanDeleteController.java
    │   │   ├── ProductionPlanListController.java
    │   │   └── ProductionPlanSearchController.java
    │   ├── schedule/
    │   │   └── ScheduleCreateController.java
    │   └── scheduleemployee/
    │       └── ScheduleEmployeeCreateController.java
    ├── dal/
    │   ├── UserDBContext.java
    │   ├── EmployeeDBContext.java
    │   ├── DepartmentDBContext.java
    │   ├── PlanDBContext.java
    │   ├── PlanCampaignDBContext.java
    │   ├── ScheduleCampaignDBContext.java
    │   └── ScheduleEmployeeDBContext.java
    ├── entity/
    │   ├── accesscontrol/
    │   │   ├── User.java
    │   │   ├── Role.java
    │   │   └── Feature.java
    │   ├── productionplan/
    │   │   ├── Department.java
    │   │   ├── Plan.java
    │   │   ├── PlanCampaign.java
    │   │   └── Product.java
    │   └── schedule/
    │       ├── ScheduleCampaign.java
    │       └── Schedule_Detail.java
    ├── web/
    │   ├── production_dashboard.jsp
    │   └── scheduleCampaign.jsp
    └── WEB-INF/
        └── web.xml

---
+++ sqlquery/SQLQuery12.sql
@@ -0,0 +1,14 @@
+﻿EXEC sp_rename 'dbo.SchedualCampaign', 'ScheduleCampaign';
+GO
+
+-- Đổi tên khóa chính và các ràng buộc liên quan
+EXEC sp_rename 'PK_SchedualCampaign', 'PK_ScheduleCampaign', 'OBJECT';
+GO
+
+-- Cập nhật các ràng buộc khóa ngoại liên quan
+ALTER TABLE [dbo].[SchedualEmployee] DROP CONSTRAINT [FK_SchedualEmployee_SchedualCampaign];
+GO
+
+ALTER TABLE [dbo].[SchedualEmployee] ADD CONSTRAINT [FK_SchedualEmployee_ScheduleCampaign] FOREIGN KEY([ScID])
+REFERENCES [dbo].[ScheduleCampaign] ([ScID]);
+GO
